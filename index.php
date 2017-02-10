073535e6db692b7a00de221c777e5e22d86a8cc2
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-02-10 14:29:07 +0700

    validate images

--- index.php
+++ index.php
@@ -1,4 +1,31 @@
 <!-- IMG_0649.JPG -->
+<?php 
+	$base_url    = 'http://localhost/14-02-2017';
+	$base_path   = 'D:\www\html\14-02-2017\images';
+	$image_files = scandir(__DIR__."/images");
+
+	$images = [];
+	foreach ($image_files as $file_names) {
+		# code...
+		if(is_image($base_path."/{$file_names}"))
+			$images[] = $file_names;
+	}
+
+	function is_image($path){
+	    $a = @getimagesize($path);
+	    $image_type = $a[2];
+	    
+	    $allowed_typed = [IMAGETYPE_JPEG, IMAGETYPE_PNG];
+
+	    if(in_array($image_type , $allowed_typed)){
+	        return true;
+	    }
+
+	    return false;
+	}
+
+	var_dump($images);
+?>
 <!DOCTYPE html>
 <html>
 <head id="Head1">
@@ -13,7 +40,17 @@ Chạy theo một dải ngân hà
 Cho tình đẹp mãi mặn mà yêu thương">
 <meta id="fbImage"       property="og:image"       content="https://tinker.press/images/make-you-smile-2.jpg">
 <meta id="fbSiteName"    property="og:site_name"   content="tinker.press">
+<style type="text/css">
+	body {
+		width: 100%;
+		height: 100%;
+		background-color: black;
+	}
+</style>
 </head>
 <body>
+<script type="text/javascript">
+	
+</script>
 </body>
 </html>
