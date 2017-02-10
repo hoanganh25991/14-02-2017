0a8f085a2999e3a95a79efbcf11d433f49d693f1
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-02-10 14:18:53 +0700

    init

---
+++ index.php
@@ -0,0 +1,19 @@
+<!-- IMG_0649.JPG -->
+<!DOCTYPE html>
+<html>
+<head id="Head1">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>tt meo con</title>
+<meta id="fbUrl"         property="og:url"         content="https://tinker.press/hh.html">
+<meta id="fbType"        property="og:type" content="article">
+<meta id="fbTitle"       property="og:title"       content="Voi con ft. Meo con ＼＿ヘ(ᐖ◞)､">
+<meta id="fbDescription" property="og:description" content="Anh vẫn ước, bình minh mỗi sáng
+Được nhìn em ngắm dáng kiêu xa
+Chạy theo một dải ngân hà
+Cho tình đẹp mãi mặn mà yêu thương">
+<meta id="fbImage"       property="og:image"       content="https://tinker.press/images/make-you-smile-2.jpg">
+<meta id="fbSiteName"    property="og:site_name"   content="tinker.press">
+</head>
+<body>
+</body>
+</html>
