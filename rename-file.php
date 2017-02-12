c7763631d9424d3167d643fe895718bb50b8b5ab
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-02-12 13:54:23 +0700

    rename file

---
+++ rename-file.php
@@ -0,0 +1,13 @@
+<?php 
+$base_url    = 'http://localhost/14-02-2017';
+// $base_path   = 'D:\www\html\14-02-2017\images';
+$base_path   = 'D:\www\html\14-02-2017\resized';
+$image_files = scandir(__DIR__."/resized");
+echo count($image_files);
+$count = 1;
+foreach ($image_files as $index => $file_name) {
+	exec("mv {$base_path}/{$file_name} {$base_path}/{$count}.jpg");
+	$count++;
+}
+die;
+?>
